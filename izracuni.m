903c6109536ee6a889c931afa70a6feab6d7f1ef
Spela Bernardic <a.spela.bernardic@gmail.com>
2024-06-03 09:15:26 +0200

    uni3 - st zetonov

--- izracuni.m
+++ izracuni.m
@@ -1,4 +1,3 @@
-
 %% Price impact
 X = 50;   % X = 5000;
 Y = 5 * 10^4;    %Y = 5 * 10^6;
@@ -31,3 +30,57 @@ dY1 = k/(X1 +dX) - Y1 % predznak od dY je -
 
 razlika = dY - dY1;
 zdrs = p0 - p1
+
+
+%% uniswap 3
+Xr = 50 % 2000;   % X = 5000;                  % realne rezerve
+Yr = 5 * 10^4 %1  ;    %Y = 5 * 10^6;        % realne rezerve
+% virtualne rezerve  
+% Xv = L/sqrt(Pb)
+% Yv = L*sqrt(Pa)
+% P0 =1000
+Pa = 800;
+Pb = 1200;
+
+
+% Xr = 1;
+% Yr = 2000;
+% Pa = 1800;
+% Pb = 2200;
+% P0 = 2000
+
+
+x = Xr;
+y = Yr;
+
+funkcija = @(L) (x + L/sqrt(Pb)) * (y + L*sqrt(Pa)) - L^2;
+
+L = fzero(funkcija, 70000)
+
+% denimo da je cena y zrasla -> mi imamo samo se x kovance
+% koliko kovancev x imamo?
+y = 0;
+x = Xr;
+
+
+funkcija1 = @(x) (x + L./sqrt(Pb)) .* (y + L.*sqrt(Pa)) - L.^2;
+
+st_x = fzero(funkcija1, 50) 
+% pravilen odgovor : having a bit over 2 ETH and 0 USDC
+
+% najin izračun: 2.056614470663034
+
+
+% kaj pa če cena kovanca y padla -> mi imamo samo se kovance y
+% koliko kovancev y imamo?
+x = 0;
+y = Yr;
+
+funkcija1 = @(y) (x + L./sqrt(Pb)) .* (y + L.*sqrt(Pa)) - L.^2;
+
+st_y = fzero(funkcija1, 50) 
+
+% pravilen odgovor: If the ETH price increases to $2200, 
+% you are fully in USDC and have apx. $4100 USDC in the LP position
+
+% najin izračun: 4.092611122562796e+03
